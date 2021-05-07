b9fbabc0d497b75f18e165d82234918133cd565d
Itachi-rgb <81573944+Itachi-rgb@users.noreply.github.com>
2021-05-07 11:21:34 -0500

    Add files via upload

---
+++ ejemplo2.py
@@ -0,0 +1,12 @@
+a = 12.2
+b = 34.1
+x = 20
+y = 10
+c = a + b
+z = x + y
+r = x- y 
+s = a - b
+print("La Sumna de Numeros Reales: ", c)
+print("La Sumna de Numeros Enteros: ", z)
+print("La Resta de Numeros Enteros: ", r)
+print("La Resta de Numeros Reales: ", s)
